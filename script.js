4c1b4984046e10670c605b692c236409f8d0835e
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-04 11:26:20 -0300

    Conclusão do Requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Requisito 4 - Referências consultadas: https://www.w3schools.com/js/js_random.asp e https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+// Requisito 4 - Referências consultadas: https://www.w3schools.com/js/js_random.asp, https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/floor e https://developer.mozilla.org/pt-BR/docs/Web/API/GlobalEventHandlers/onload
 
 window.onload = carregandoCoresIniciais;
 
@@ -10,9 +10,25 @@ function coresIniciais() {
    return rgb
 } 
 
-function carregandoCoresIniciais() {
+let textoCharada = document.getElementById("rgb-color");
+let respostaMostrada = document.getElementById("answer");
 let bolas = document.getElementsByClassName("ball"); 
-for (let indice = 0; indice < bolas.length; indice += 1) {
-bolas[indice].style.backgroundColor = coresIniciais()
+for (let index = 0; index < bolas.length; index += 1)
+{bolas[index].addEventListener('click', checkResposta)}
+
+function carregandoCoresIniciais() {
+for (let indice = 0; indice < bolas.length; indice += 1) 
+{bolas[indice].style.backgroundColor = coresIniciais()}
+let random = Math.floor(Math.random() * 5);
+textoCharada.innerText = bolas[random].style.backgroundColor;
 }
+
+// Requisito 5
+function checkResposta(event) { 
+let bolaClicada = event.target;
+let respostaUser = bolaClicada.style.backgroundColor;
+if (textoCharada.innerText === respostaUser)
+{respostaMostrada.innerText = "Acertou!"}
+else 
+{respostaMostrada.innerText = "Errou! Tente novamente!"} 
 }
